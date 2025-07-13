d3b1472cd45b69ff77e5ae9c42a5492a5131cdd8
Forbium <124632079+Forbium@users.noreply.github.com>
2025-07-13 20:08:33 +0200

    Add files via upload

--- README_EN.txt
+++ README_EN.txt
@@ -2,19 +2,6 @@ This addon was written by Forbium and Rogolist.
 The Whisper Bind addon is written so that you can whisper to a specific player by pressing buttons without occupying
 a separate slot for the macro.
 
-<<<<<<< HEAD
-Usage.
-To configure whispering, you need to go to the addon settings by clicking on the “settings” button, it will open a
-window where there will be three columns with fields. In the first column we write what we want the button to be called
-(to make it easy to find), in the second, who we want to send the message to, and in the third, what kind of message we
-want to send. After completing the settings, you need to click the “save” button and you can close the settings window.
-Now the next time your account is launched, you will not need to enter this information into the settings, it will be
-automatically loaded.
-
-Well suited for the Whispercast addon if you do not need to send many different messages.
-If the window goes beyond the screen, you need to write the command /run whisperbind:SetPoint("TOPLEFT",600,-200);.
-This will bring the window to the center of the screen.
-=======
 Usage:
 To configure the whisper, you need to open the addon settings window by clicking on the "options" button, after
 clicking it, a window will open in which there will be 4 columns:
@@ -45,4 +32,3 @@ you right-click on the "categories" button, a list of all available categories w
 You can also use some variables in messages: %player%, %myname%, %time%, %zone%, %guild%, %level%, %class%
 
 Well suited for the Whispercast addon if you do not need to send many different messages.
->>>>>>> origin/main
--- whisperbind.lua
+++ whisperbind.lua
@@ -1,24 +1,12 @@
-<<<<<<< HEAD
-=======
 -- Enhanced WhisperBind v2.0 для WoW Turtle (1.12.1)
 -- Расширенная версия с категориями, переменными, статистикой и автоответчиком
 
->>>>>>> origin/main
 wbopt = 0
 wbcount = nil
 
 WB_buttonbox = {}
 WB_playerbox = {}
 WB_messagebox = {}
-<<<<<<< HEAD
-
---DEFAULT_CHAT_FRAME:AddMessage(saveinformation[1], 1.0, 1.0, 0.0)
-
--- https://wowwiki-archive.fandom.com/wiki/USERAPI_SlashCmdList_AddSlashCommand
--- /forbium
-
--- Rogolist edition 2024.01.11
-=======
 WB_categories = WB_categories or {}
 WB_icons = {}
 WB_usage_stats = {}
@@ -42,76 +30,10 @@ WB_autoresponder.responses = WB_autoresponder.responses or {}
 WB_autoresponder.keywords = WB_autoresponder.keywords or {}
 WB_autoresponder.ignore_list = WB_autoresponder.ignore_list or {}
 WB_autoresponder.enabled_rows = WB_autoresponder.enabled_rows or {}
->>>>>>> origin/main
 
 
 
 
-<<<<<<< HEAD
-function wbinterface_OnLoad()
-    this:RegisterEvent("ADDON_LOADED");
-
-    DEFAULT_CHAT_FRAME:AddMessage("WhisperBind is loaded", 1.0, 1.0, 0.0)
-    WBInterfaceFrame:Show()
-    --options:Hide()
-    --wbopt = 0;
-	--WBInterfaceFrame_Resize(wbcount)
-	--defaultVariables()
-end
-
-
--- https://wowwiki-archive.fandom.com/wiki/Saving_variables_between_game_sessions
-function wbinterface_OnEvent(event)
-	if (event == "ADDON_LOADED") then
-		--addons_massiv[iii] = ("LOADED: === "..arg1)
-		--iii = iii + 1;
-		--DEFAULT_CHAT_FRAME:AddMessage("LOADED: === "..arg1, 1.0, 1.0, 0.0)
-		if (arg1 == "!WhisperBind") then
-			DEFAULT_CHAT_FRAME:AddMessage("WhisperBind variables loaded", 1.0, 1.0, 0.0)
-			wbopt = 0;
-			if (wbcount == nil) then wbcount = 3 end
-			
-			WBInterfaceFrame_Resize(wbcount)
-			wbinterface_make_buttons();
-			wbinterface_make_editboxes();
-			WB_loadvalue();
-		end
-	end
-end
-
-
-function WBClickButton(id)
-	--local text = clicked_button:GetText()
-	id = id - 5
-	--DEFAULT_CHAT_FRAME:AddMessage("id = "..id, 1.0, 1.0, 0.0)
-	--DEFAULT_CHAT_FRAME:AddMessage("player = "..WB_playerbox[id], 1.0, 1.0, 0.0)
-	--DEFAULT_CHAT_FRAME:AddMessage("message = "..WB_messagebox[id], 1.0, 1.0, 0.0)
-	
-	local player, text = WB_playerbox[id], WB_messagebox[id]
-	--DEFAULT_CHAT_FRAME:AddMessage("player = "..player, 1.0, 1.0, 0.0)
-	--DEFAULT_CHAT_FRAME:AddMessage("text = "..text, 1.0, 1.0, 0.0)
-	
-	--if (string.len(player)>1) and (string.len(text)>1) then
-	if (string.len(player)>1) then
-		SendChatMessage(text, "WHISPER", nil, player);
-	end
-end
-
-
-function WB_optionsbtn()
-    if(wbopt == 0) then
-		--DEFAULT_CHAT_FRAME:AddMessage("show", 1.0, 1.0, 0.0)
-        WBOptionsFrame:Show()
-        --SaveFrame:Show()
-        wbopt = 1;
-
-    else
-		--DEFAULT_CHAT_FRAME:AddMessage("hide", 1.0, 1.0, 0.0)
-        WBOptionsFrame:Hide()
-        --SaveFrame:Hide()
-        wbopt = 0;
-
-=======
 -- UI элементы
 WB_category_dropdown = nil
 WB_stats_frame = nil
@@ -403,105 +325,10 @@ function WB_HandleIncomingWhisper(message, sender)
                 end
             end
         end
->>>>>>> origin/main
     end
 end
 
 
-<<<<<<< HEAD
-
--- Rogolist edition
-function WB_savevalue()
-
-	for ii = 1,wbcount do
-		
-
-		WB_buttonbox[ii] = WB_editbox[ii]:GetText()
-		--DEFAULT_CHAT_FRAME:AddMessage("WB_buttonbox["..ii.."] = "..WB_editbox[ii]:GetText(), 1.0, 1.0, 0.0)
-		local b=ii+5
-		WB_button[b]:SetText(WB_buttonbox[ii])
-		
-		local p = ii + wbcount
-		WB_playerbox[ii] = WB_editbox[p]:GetText()
-		--DEFAULT_CHAT_FRAME:AddMessage("WB_playerbox["..ii.."] = "..WB_editbox[p]:GetText(), 1.0, 1.0, 0.0)
-
-		local m = ii + wbcount*2
-		WB_messagebox[ii] = WB_editbox[m]:GetText()
-		--DEFAULT_CHAT_FRAME:AddMessage("WB_mesagebox["..ii.."] = "..WB_editbox[m]:GetText(), 1.0, 1.0, 0.0)
-	end
-	
-
-end
-
-function WB_loadvalue()
-	
-	WB_button[1]:SetText("Options");
-	WB_button[2]:SetText("Save");
-	WB_button[3]:SetText("ButtonID");
-	WB_button[4]:SetText("Player");
-	WB_button[5]:SetText("Message");
-
-	for ii = 1,wbcount do
-		local b = ii+5
-		--WB_button[b]:SetText("Test");
-		WB_button[b]:SetText(WB_buttonbox[ii]);
-		--DEFAULT_CHAT_FRAME:AddMessage("WB_button["..b.."] = "..WB_buttonbox[ii], 1.0, 1.0, 0.0)
-		
-		--WB_editbox[ii]:SetText("Name");
-		WB_editbox[ii]:SetText(WB_buttonbox[ii]);
-		--DEFAULT_CHAT_FRAME:AddMessage("WB_editbox["..ii.."] = "..WB_buttonbox[ii], 1.0, 1.0, 0.0)
-		local p = ii + wbcount
-		--WB_editbox[p]:SetText("Play");
-		WB_editbox[p]:SetText(WB_playerbox[ii]);
-		--DEFAULT_CHAT_FRAME:AddMessage("WB_editbox["..p.."] = "..WB_playerbox[ii], 1.0, 1.0, 0.0)
-		local m = ii + wbcount*2
-		--WB_editbox[m]:SetText("Mess");
-		WB_editbox[m]:SetText(WB_messagebox[ii]);
-		--DEFAULT_CHAT_FRAME:AddMessage("WB_editbox["..m.."] = "..WB_messagebox[ii], 1.0, 1.0, 0.0)
-	end
-end
-
-function WB_debug()
-	local yy =1
-	while WB_button[yy] do
-		WB_button[yy]:SetText("Butt_"..yy);
-		yy = yy + 1
-	end
-	
-	local yy =1
-	while WB_editbox[yy] do
-		WB_editbox[yy]:SetText("EBox_"..yy);
-		yy = yy + 1
-	end
-	
-end
-
-function WBdefaultVariables()
-	for i=1,wbcount do
-		WB_buttonbox[i] = "name"..i
-		WB_playerbox[i] = "play"..i
-		WB_messagebox[i] = "mess"..i
-	end
-end
-
-
-function WB_interface_show()
-	DEFAULT_CHAT_FRAME:AddMessage("BUTTONS:", 1.0, 1.0, 0.0)
-	for i,k in pairs(WB_buttonbox) do
-		if (k ~= nil) then DEFAULT_CHAT_FRAME:AddMessage(i.." - "..k, 1.0, 1.0, 0.0) end
-	end
-	
-	DEFAULT_CHAT_FRAME:AddMessage("PLAYERS:", 1.0, 1.0, 0.0)
-	for i,k in pairs(WB_playerbox) do
-		if (k ~= nil) then DEFAULT_CHAT_FRAME:AddMessage(i.." - "..k, 1.0, 1.0, 0.0) end
-	end
-	
-	DEFAULT_CHAT_FRAME:AddMessage("MESSAGES:", 1.0, 1.0, 0.0)
-	for i,k in pairs(WB_messagebox) do
-		if (k ~= nil) then DEFAULT_CHAT_FRAME:AddMessage(i.." - "..k, 1.0, 1.0, 0.0) end
-	end
-	
-=======
 -- Запланированная отправка сообщения
 function WB_ScheduleMessage(player, message, delay)
     local start_time = GetTime()
@@ -642,13 +469,10 @@ SlashCmdList["WBAUTO"] = function()
     WB_autoresponder.enabled = not WB_autoresponder.enabled
     local status = WB_autoresponder.enabled and "включен" or "отключен"
     DEFAULT_CHAT_FRAME:AddMessage("[WB]:Автоответчик " .. status, 1.0, 1.0, 0.0)
->>>>>>> origin/main
 end
 
 
 
-<<<<<<< HEAD
-=======
 
 
 -- Добавьте эти функции в whisperbind.lua
@@ -883,4 +707,3 @@ SlashCmdList["WBCAT"] = function(msg)
         DEFAULT_CHAT_FRAME:AddMessage("/wb import [string] - import settings", 0.8, 0.8, 1.0)
     end
 end
->>>>>>> origin/main
